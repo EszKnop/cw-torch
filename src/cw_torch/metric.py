5b6b8acfb8ce02c56af32f42472466a9e63fced1
szymon.knop <szymonknop@gmail.com>
2022-06-20 20:12:49 +0200

    0.4.0 - Added normality measure implementation that does not perform scaling by const.

--- src/cw_torch/metric.py
+++ src/cw_torch/metric.py
@@ -1,12 +1,16 @@
 import torch
 
-def cw_normality(sample: torch.Tensor, gamma: torch.Tensor) -> torch.Tensor:
+cw_normality_scale_factor = 0.28209479177
+
+def cw_normality_no_scale(sample: torch.Tensor, gamma: torch.Tensor) -> torch.Tensor:
     """The function measures a Cramer-Wold distance of a sample from the standard multivariate normal distribution. 
     The implementation follows the formula from Theorem 3 in: https://jmlr.org/papers/volume21/19-560/19-560.pdf
+    This function skips scaling of the computed distance by $ \frac{1}{2*sqrt()\pi) }$. 
+    It is effectively the formula from page 14 which is the formula used by original CWAE implementation.
 
     Args:
         sample (torch.Tensor): sample to evaluate. Sample must be a 2D tensor N x D.
-        gamma (torch.Tensor, optional): value of gamma coefficient. 
+        gamma (torch.Tensor): value of gamma coefficient. 
 
     Returns:
         torch.Tensor: One dimensional nonnegative tensor storing a distance of the sample from standard multidimensional normal distribution.
@@ -14,15 +18,33 @@ def cw_normality(sample: torch.Tensor, gamma: torch.Tensor) -> torch.Tensor:
     assert sample.ndim == 2, f"{sample} must be a two dimensional tensor"
     assert gamma.ndim == 0, f"{gamma} must be a zero dimensional tensor"
 
-    D = sample.size(1)
+    N, D = sample.size()
 
     K = 1.0/(2.0*D-3.0)
 
-    A1 = torch.cdist(sample, sample)**2
-    A = torch.rsqrt(gamma + K*A1).mean()
+    A1 = torch.pdist(sample)**2
+    A = 2.0 * torch.rsqrt(gamma + K*A1).sum() / N**2 + torch.rsqrt(gamma) / N
 
     B1 = torch.linalg.norm(sample, 2, axis=1)**2 
-    B = 2*(torch.rsqrt(gamma + 0.5 + K*B1)).mean()
+    B = torch.rsqrt(gamma + 0.5 + K*B1).mean()
 
-    return torch.rsqrt(1+gamma) + A - B
+    result = A + torch.rsqrt(1+gamma) - 2.0 * B
+
+    return result
+
+def cw_normality(sample: torch.Tensor, gamma: torch.Tensor) -> torch.Tensor:
+    """The function measures a Cramer-Wold distance of a sample from the standard multivariate normal distribution. 
+    The implementation follows the formula from Theorem 3 in: https://jmlr.org/papers/volume21/19-560/19-560.pdf
+
+    Args:
+        sample (torch.Tensor): sample to evaluate. Sample must be a 2D tensor N x D.
+        gamma (torch.Tensor): value of gamma coefficient. 
+
+    Returns:
+        torch.Tensor: One dimensional nonnegative tensor storing a distance of the sample from standard multidimensional normal distribution.
+    """
+    
+    result = cw_normality_no_scale(sample, gamma)   
+
+    return result * cw_normality_scale_factor
 
--- tests/normality_measure_tests.py
+++ tests/normality_measure_tests.py
@@ -5,16 +5,44 @@ from src.cw_torch.metric import cw_normality
 
 class TestNormalityMeasure(unittest.TestCase):
 
+    def test_zero_vector_and_ones_vector(self):
+        N, D = (2, 32)
+        gamma = torch.tensor(0.5)
+        sample = torch.zeros((N, D))
+        sample[1, :] = torch.ones((1, D))
+
+        normality_distance = cw_normality(sample, gamma)
+        
+        self.assertAlmostEqual(normality_distance.item(), 0.05858594277, places=5)
+   
     def test_distance_for_standard_normal_samples_are_smaller_than_these_for_uniform_ones_when_using_the_same_gamma(self):
         N, D = (100, 40)
         standard_sample = torch.randn((N, D))
         uniform_sample = torch.rand((N, D))
-        gamma = torch.ones((1, ))
+        gamma = torch.tensor(1.0)
 
         standard_value = cw_normality(standard_sample, gamma)
         uniform_value = cw_normality(uniform_sample, gamma)
         
         self.assertLess(standard_value, uniform_value)
+
+    def test_result_is_zero_dim_tensor(self):
+        N, D = (16, 128)
+        gamma = torch.tensor(0.2)
+        sample = torch.randn((N, D))
+
+        normality_distance = cw_normality(sample, gamma)
+        
+        self.assertEqual(normality_distance.ndim, 0)
+
+    def test_behavior_for_sample_containing_only_zeros(self):
+        N, D = (8, 32)
+        gamma = torch.tensor(1.0)
+        sample = torch.zeros((N, D))
+
+        normality_distance = cw_normality(sample, gamma)
+        
+        self.assertAlmostEqual(normality_distance.item(), 0.07411361933*0.28209479177, places=5)
 
 if __name__ == '__main__':
     unittest.main()
